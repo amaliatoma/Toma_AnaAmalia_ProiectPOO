afbd2f50bf613e190519b55b67f65f2838429027
Amalia Toma <126448005+amaliatoma@users.noreply.github.com>
2023-11-04 17:40:14 +0200

    Update proiect

--- teatru2.cpp
+++ teatru2.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 using namespace std;
 
@@ -12,7 +12,7 @@ private:
 	int nrAngajati;
 	static double reducereStudenti;
 public:
-	// getteri 
+	// geteri 
 	char* getNume() {
 		return this->nume;
 	}
@@ -35,7 +35,7 @@ public:
 		return reducereStudenti;
 	}
 
-	//setteri
+	//seteri
 	void setNume(char* nume) {
 		if (this->nume != NULL) {
 			delete[]this->nume;
@@ -62,6 +62,7 @@ public:
 	{
 		reducereStudenti = reducereNoua;
 	}
+	// metoda de afisare
 	void afisare() {
 		cout << "Nume teatru : " << this->nume << endl;
 		cout << "Adresa: " << this->adresa << endl;
@@ -72,7 +73,7 @@ public:
 		cout << "Reducere studenti: " << this->reducereStudenti << endl;
 		cout << endl;
 	}
-
+	// constructor FARA parametrii
 	Teatru() : an_infiintare(1911) {
 		this->nume = new char[strlen("Odeon") + 1];
 		strcpy_s(this->nume, strlen("Odeon") + 1, "Odeon");
@@ -81,7 +82,7 @@ public:
 		this->nrSali = 10;
 		this->nrAngajati = 50;
 	}
-
+	//constructor CU TOTI parametrii
 	Teatru(const char* nume, string adresa, const int an, int capacitateMax, int nrSali,int nrAngajati) : an_infiintare(an) {
 		this->nume = new char[strlen(nume) + 1];
 		strcpy_s(this->nume, strlen(nume) + 1, nume);
@@ -90,6 +91,7 @@ public:
 		this->nrSali = nrSali;
 		this->nrAngajati = nrAngajati;
 	}
+	// constructor CU O PARTE din parametrii
 	Teatru(const char* nume, string adresa, const int an) : an_infiintare(an), capacitateMax(0), nrSali(0), nrAngajati(0) {
 		this->nume = new char[strlen(nume) + 1];
 		strcpy_s(this->nume, strlen(nume) + 1, nume);
@@ -104,11 +106,51 @@ public:
 		this->nrSali = teatru.nrSali;
 		this->nrAngajati = teatru.nrAngajati;
 	}
-
+	// metoda -> calculeaza capacitatea per sala
 	int capacitateSala() {
 		int capacitateSala = this->capacitateMax / this->nrSali;
 		return capacitateSala;
 	}
+
+	friend void TotalNrAngajati(Teatru teatru1, Teatru teatru2);
+
+	// operator '=' 
+	Teatru& operator=(const Teatru& teatru) {
+		if (this != &teatru) {
+			if (this->nume != NULL) {
+				delete[]this->nume;
+			}
+			this->nume = new char[strlen(teatru.nume) + 1];
+			strcpy_s(this->nume, strlen(teatru.nume) + 1, teatru.nume);
+			this->adresa = teatru.adresa;
+			this->capacitateMax = teatru.capacitateMax;
+			this->nrSali = teatru.nrSali;
+			this->nrAngajati = teatru.nrAngajati;
+		}
+		return *this;
+	}
+
+	// functie prietena, NU METODA 
+	// operator '<<'
+	friend ostream& operator<< (ostream& consola, const Teatru& teatru) {
+		consola << "Nume: " << teatru.nume<< endl;
+		consola << "Adresa: " << teatru.adresa<<endl;
+		consola << "An infiintare: " << teatru.an_infiintare<<endl;
+		consola << "Capacitate Maxima: " << teatru.capacitateMax<< endl;
+		consola << "Numar sali: " << teatru.nrSali<<endl;
+		consola << "Numar angajati: " << teatru.nrAngajati<<endl;
+		consola << "Reducere Studenti: " << teatru.reducereStudenti << endl;
+		consola << endl;
+		return consola;
+	}
+
+	friend bool operator< (Teatru teatru1, Teatru teatru2);
+
+	// operator -
+	friend int operator- (Teatru teatru1, Teatru teatru2) {
+		return teatru1.nrSali - teatru2.nrSali;
+	}
+
 	~Teatru() {
 		if (this->nume != NULL) {
 			delete this->nume;
@@ -117,6 +159,11 @@ public:
 
 };
 double Teatru::reducereStudenti = 0.75;
+
+// operator < 
+bool operator< (Teatru teatru1, Teatru teatru2) {
+	return teatru1.capacitateMax < teatru2.capacitateMax;
+}
 
 class Piesa {
 private:
@@ -168,7 +215,7 @@ public:
 	void setBileteLuate(int BileteLuate) {
 		this->BileteLuate = BileteLuate;
 	}
-
+	// metoda afisare 
 	void afisare() {
 		cout << "Nume piesa: " << this->nume << endl;
 		cout << "Producator: " << this->scriitor << endl;
@@ -179,6 +226,7 @@ public:
 		cout << endl;
 
 	}
+	//constructor FARA parametrii
 	Piesa() : scriitor("Bogdan Olteanu") {
 		this->nume = "Julieta fara Romeo";
 		this->data = new char[strlen("29.10.2023") + 1];
@@ -186,6 +234,7 @@ public:
 		this->pretBilet = 99.99;
 		this->BileteLuate = 40;
 	}
+	// constructor CU TOTI parametrii
 	Piesa(string nume, const string scriitor, const char* data, double pretBilet, int BileteLuate) : scriitor(scriitor) {
 		this->nume = nume;
 		this->data = new char[strlen(data) + 1];
@@ -193,6 +242,7 @@ public:
 		this->pretBilet = pretBilet;
 		this->BileteLuate = BileteLuate;
 	}
+	// constructor parametrii PARTIAL
 	Piesa(string nume, const string scriitor, const char* data) : scriitor(scriitor), pretBilet(10.89), BileteLuate(0) {
 		this->nume = nume;
 		this->data = new char[strlen(data) + 1];
@@ -206,10 +256,66 @@ public:
 		this->pretBilet = piesa.pretBilet;
 		this->BileteLuate = piesa.BileteLuate;
 	}
-
+	// metoda calculeaza casieria de la vanzarea biletelor: 
 	void sold() {
 		cout << "Suma obtinuta de la vanzarea biletelor: " << this->pretBilet * this->BileteLuate << " de lei." << endl;
 	}
+
+	friend void TotalBilete(Piesa piesa1, Piesa piesa2);
+
+	//operator '='
+	Piesa operator=(Piesa piesa) {
+		if (this != &piesa) {
+			this->nume = piesa.nume;
+			if (this->data != NULL) {
+				delete[]this->data;
+			}
+			this->data = new char[strlen(piesa.data) + 1];
+			strcpy_s(this->data, strlen(piesa.data) + 1, piesa.data);
+			this->pretBilet = piesa.pretBilet;
+			this->BileteLuate = piesa.BileteLuate;
+		}
+		return *this;
+	}
+	// operator '>>'
+	friend istream& operator>> (istream& tastatura, Piesa& piesa) {
+		
+		cout << "Nume: ";
+		tastatura >> piesa.nume;
+
+		if (piesa.data != NULL)
+		{
+			delete[]piesa.data;
+		}
+		char data[10];
+		cout << "Data: ";
+		tastatura >> data;
+		piesa.data = new char[strlen(data)+1];
+		strcpy_s(piesa.data, strlen(data) + 1, data);
+		cout << "Pret Bilet: ";
+		tastatura >> piesa.pretBilet;
+		cout << "Bilete Luate: ";
+		tastatura >> piesa.BileteLuate;
+		return tastatura;
+		
+	}
+	// operator ++ preincrementare 
+	Piesa operator++ () {
+		this->pretBilet = this->pretBilet + 16;
+		return *this;
+	}
+
+	//operator ++ postincrementare
+	Piesa operator++ (int) {
+		Piesa temp = *this;
+		this->pretBilet= this->pretBilet + 16;
+		return temp;
+	}
+	// operator '!='
+	friend bool operator!=(Piesa piesa1, Piesa piesa2) {
+		return piesa1.pretBilet != piesa2.pretBilet;
+	}
+
 	~Piesa() {
 		if (this->data != NULL) {
 			delete[] this->data;
@@ -269,7 +375,7 @@ public:
 	static void setImpozitPeSalariu(double impozitNou) {
 		impozitPeSalariu = impozitNou;
 	}
-
+	// metoda de afisare
 	void afisare() {
 		cout << "Nume: " << this->nume << endl;
 		cout << "An nastere: " << this->an_nastere << endl;
@@ -279,7 +385,7 @@ public:
 		cout << "Impozit pe salariu: " << this->impozitPeSalariu << endl;
 		cout << endl;
 	}
-
+	//constructor FARA parametrii
 	Actor() :an_nastere(1963) {
 		this->nume = "Brad Pitt";
 		this->nationalitate = new char[strlen("SUA") + 1];
@@ -287,6 +393,7 @@ public:
 		this->filmPtCareECunoscut = "Fight Club";
 		this->salariuBrut = 170000;
 	}
+	//constructor CU TOTI parametrii
 	Actor(string nume, const int an, const char* nationalitate, string filmPtCareECunoscut, double salariuBrut) :an_nastere(an) {
 		this->nume = nume;
 		this->nationalitate = new char[strlen(nationalitate) + 1];
@@ -294,6 +401,7 @@ public:
 		this->filmPtCareECunoscut = filmPtCareECunoscut;
 		this->salariuBrut = salariuBrut;
 	}
+	// constructor parametrii PARTIAL
 	Actor(string nume, const int an, const char* nationalitate) :an_nastere(an), filmPtCareECunoscut("Breakfast at Tiffany's"), salariuBrut(220000) {
 		this->nume = nume;
 		this->nationalitate = new char[strlen(nationalitate) + 1];
@@ -307,38 +415,85 @@ public:
 		this->filmPtCareECunoscut = actor.filmPtCareECunoscut;
 		this->salariuBrut = actor.salariuBrut;
 	}
-
+	// metoda -> salariul net
 	void salariuNet() {
 		double salariuNet = this->salariuBrut - (this->impozitPeSalariu * this->salariuBrut);
 		cout << "Salariul Net este: " << salariuNet<< endl;
 	}
+	// operator '='
+	Actor operator=(Actor actor) {
+		if (this != &actor) {
+			this->nume = actor.nume;
+			if (this->nationalitate != NULL) {
+				delete[]this->nationalitate;
+			}
+			this->nationalitate = new char[strlen(actor.nationalitate) + 1];
+			strcpy_s(this->nationalitate, strlen(actor.nationalitate) + 1, actor.nationalitate);
+			this->filmPtCareECunoscut = actor.filmPtCareECunoscut;
+			this->salariuBrut = actor.salariuBrut;
+		}
+		return *this;
+	}
+	// operator -- pre
+	Actor operator--() {
+		this->salariuBrut = this->salariuBrut - 400;
+		return *this;
+	}
+	//operator -- post
+	Actor operator--(int) {
+		Actor temp = *this;
+		this->salariuBrut = this->salariuBrut - 400;
+		return temp;
+	}
+	// operator '>'
+	friend bool operator>(Actor actor1, Actor actor2) {
+		return actor1.salariuBrut > actor2.salariuBrut;
+	}
+
+	friend int operator-= (Actor actor1, Actor actor2);
 
 	~Actor() {
 		if (this->nationalitate != NULL) {
 			delete[]this->nationalitate;
 		}
 	}
-
 };
 double Actor::impozitPeSalariu = 0.16;
+// operator '-='
+int operator-= (Actor actor1, Actor actor2) {
+	actor1.salariuBrut = actor1.salariuBrut - actor2.salariuBrut;
+	return actor1.salariuBrut;
+}
 
-// functie globala 1
+
+// functie globala - foloseste clasa piesa
+void TotalBilete (Piesa piesa1, Piesa piesa2) {
+	cout << "Nr bilete luate in total la cele doua piese este " << piesa1.BileteLuate + piesa2.BileteLuate;
+}
+
+// functie globala - foloseste clasa teatru
+void TotalNrAngajati(Teatru teatru1, Teatru teatru2) {
+	cout << "Total numar salariati: " << teatru1.nrAngajati + teatru2.nrAngajati;
+}
+
+// functie globala 1 - foloseste 2 clase
 void locuriDisponibile(Teatru teatru, Piesa piesa) {
 	int capacitateSala = teatru.capacitateSala();
 	cout << "La piesa " << piesa.getNume() << " mai sunt disponibile " << (capacitateSala - piesa.getBileteLuate()) << " de locuri"<<endl;
 }
 
-//functie globala 2 
+//functie globala 2 - foloseste toate clasele
 void anuntPublicitar(Teatru teatru, Piesa piesa, Actor actor) {
 	cout << "Anunt Publicitar: ";
 	cout << endl<<endl;
 	cout << "      O aventura dramatica ca niciuna alta!" << endl<<endl;
-	cout << "  Piesa ' " << piesa.getNume() <<" ' aduce pe scena o poveste captivanta si un actor principal incredibil, "
+	cout << "  Piesa ' " << piesa.getNume()<<" ' aduce pe scena o poveste captivanta si un actor principal incredibil, "
 		<< actor.getNume() << endl;
 	cout << "  Teatrul " << teatru.getNume() << " va invita sa experimentati magia teatrului intr-un mod unic." << endl;
-	cout << "Pret bilet: " << piesa.getPretBilet() << " lei.";
+	cout << "Pret bilet: " << piesa.getPretBilet()<< " lei.";
 
 }
+
 // afisare cu ajutorul geterilor
 void afisareTeatruGet(Teatru teatru) {
 	cout << "Nume teatru : " << teatru.getNume() << endl;
@@ -423,5 +578,45 @@ int main()
 	afisareTeatruGet(teatru);
 	afisarePiesaGet(piesa2);
 	afisareActorGet(actor3);
+
+	cout << "--------------OPERATORI--------------" << endl;
+	Teatru teatru4;
+	teatru4 = teatru3;
+	cout << teatru4;
+
+	if (teatru < teatru2) cout << "Teatru 2 este mai mare."<<endl;
+	cout << teatru2 - teatru;
+
+	Piesa piesa4;
+	piesa4 = piesa2;
+	piesa4.afisare();
+
+	/*Piesa piesa5;
+	cin >> piesa5;
+	piesa5.afisare();*/
+	Piesa piesa6;
+	Piesa piesa7;
+	piesa7 = piesa2++;
+	piesa7.afisare();
+	piesa6 = ++piesa2;
+	piesa6.afisare();
+
+	if (piesa6 != piesa7) cout << "Cele doua nu au pret egal."<<endl;
+	else cout << "Piesele au pret egal"<<endl;
+
+
+	Actor actor4;
+	actor4 = actor3;
+	actor4.afisare();
+
+	Actor actor5;
+	Actor actor6;
+	actor5 = actor2--;
+	actor5.afisare();
+	actor6 = --actor2;
+	actor6.afisare();
+
+	if (actor > actor3) cout << "Brad Pitt castiga mai mult decat Audrey Hepburn cu"<<(actor -=actor3)<<endl;
+	else cout << "Audrey Hepburn castiga mai mult decat Brad Pitt cu "<<(actor3 -=actor)<<endl;
 
 }
